d7e5a23ac560cc866964d13ff9f5fa308c2b3312
SY <shingadesy21@gmail.com>
2020-07-07 13:12:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/clientsocket/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.clientsocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int SERVER_PORT = 5050;
+
+    public static final String SERVER_IP = "192.168.43.212";
+    private ClientThread clientThread;
+    private Thread thread;
+    private LinearLayout msgList;
+    private Handler handler;
+    private int clientTextColor;
+    private EditText edMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Client");
+        clientTextColor = ContextCompat.getColor(this, R.color.green);
+        handler = new Handler();
+        msgList = findViewById(R.id.msgList);
+        edMessage = findViewById(R.id.edMessage);
+    }
+
+    public TextView textView(String message, int color, Boolean value) {
+        if (null == message || message.trim().isEmpty()) {
+            message = "<Empty Message>";
+        }
+        TextView tv = new TextView(this);
+        tv.setTextColor(color);
+        tv.setText(message + " [" + getTime() + "]");
+        tv.setTextSize(20);
+        tv.setPadding(0, 5, 0, 0);
+        tv.setLayoutParams(new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 0));
+        if (value) {
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+        }
+        return tv;
+    }
+
+    public void showMessage(final String message, final int color, final Boolean value) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                msgList.addView(textView(message, color, value));
+            }
+        });
+    }
+
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.connect_server) {
+            msgList.removeAllViews();
+            clientThread = new ClientThread();
+            thread = new Thread(clientThread);
+            thread.start();
+            return;
+        }
+
+        if (view.getId() == R.id.send_data) {
+            String clientMessage = edMessage.getText().toString().trim();
+            showMessage(clientMessage, Color.BLUE, false);
+            if (null != clientThread) {
+                if (clientMessage.length() > 0){
+                    clientThread.sendMessage(clientMessage);
+                }
+                edMessage.setText("");
+            }
+        }
+    }
+
+    /* clientThread class defined to run the client connection to the socket network using the server ip and port
+     * and send message */
+    class ClientThread implements Runnable {
+
+        private Socket socket;
+        private BufferedReader input;
+
+        @Override
+        public void run() {
+
+            try {
+
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                showMessage("Connecting to Server...", clientTextColor, true);
+
+                socket = new Socket(serverAddr, SERVER_PORT);
+
+                if (socket.isBound()){
+
+                    showMessage("Connected to Server...", clientTextColor, true);
+                }
+
+
+                while (!Thread.currentThread().isInterrupted()) {
+
+
+                    this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String message = input.readLine();
+                    if (null == message || "Disconnect".contentEquals(message)) {
+                        Thread.interrupted();
+                        message = "Server Disconnected...";
+                        showMessage(message, Color.RED, false);
+                        break;
+                    }
+                    showMessage("Server: " + message, clientTextColor, true);
+                }
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                showMessage("Problem Connecting to server... Check your server IP and Port and try again", Color.RED, false);
+                Thread.interrupted();
+                e1.printStackTrace();
+            } catch (NullPointerException e3) {
+                showMessage("error returned", Color.RED,true);
+            }
+
+        }
+
+        void sendMessage(final String message) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (null != socket) {
+                            PrintWriter out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(socket.getOutputStream())),
+                                    true);
+                            out.println(message);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != clientThread) {
+            clientThread.sendMessage("Disconnect");
+            clientThread = null;
+        }
+    }
+}
